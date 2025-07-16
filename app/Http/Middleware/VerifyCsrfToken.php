0b21a309a900ccee0d28aa0017c9d8a46d921260
agustinaardini <agustinaardinilestari@gmail.com>
2025-07-16 21:56:46 +0700

    add

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/proseslogin' // Tambahkan ini untuk sementara waktu
+        //'/proseslogin' // Tambahkan ini untuk sementara waktu
         //
     ];
 }
