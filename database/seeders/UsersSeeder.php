6a19a97ee2464e0a59ba2faa78f4416260616a2c
agustinaardini <agustinaardinilestari@gmail.com>
2025-07-16 16:57:24 +0700

    add seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -3,24 +3,35 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class UsersSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
+        // Admin
         User::create([
-            'name'      => 'admin',
-            'email'     => 'dinii@gmail.com',
-            'password'  => Hash::make('dini123'),
+            'name' => 'admin',
+            'email' => 'dinii@gmail.com',
+            'password' => Hash::make('dini123'),
             'role' => 'admin',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+
+        // Guru 1
+        User::create([
+            'name' => 'Suluh Setya H., S.Pd',
+            'email' => 'suluhsetyah.@sekolah.sch.id',
+            'password' => Hash::make('password1'),
+            'role' => 'guru',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+
     }
 }
