2246018323c9a3d40f21fa8403f97afa55901046
agustinaardini <agustinaardinilestari@gmail.com>
2025-07-17 12:40:23 +0700

    add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
          $this->call([
             UsersSeeder::class,
-
+            RoleSeeder::class,
+            UserRoleSeeder::class
         ]);
     }
 }
